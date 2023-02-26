fd799f572ab6a5b385380f87def23cd353d668cd
Jen Cruz <maxcruz4455@gmail.com>
2023-02-27 06:33:41 +0800

    mongoose updated to strictQuery: true

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const connectDB = async()=>{
     try{
         //mongo connection  string
+        mongoose.set("strictQuery", false);
         const con = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
-            strictQuery: true
+            useNewUrlParser:true,   
         })
         console.log(`MongoDB connected:${con.connection.host}`);
     }
